6cfb48ca48045c0be822b416fbd19ab5499170b1
newusername <=>
2025-09-11 00:43:27 +0530

    merge - taken merge from live JIRA-TPW2-22580

---
+++ src/app/dashboard/purchases/component/distributor-or-invoice-list.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+
+interface DistributorOrInvoiceListProps {
+  distributorId: number;
+  title: string;
+  date: string;
+  seleceted: boolean;
+  amount?: number | string;
+  onClick?: (id: number) => void;
+}
+
+const DistributorOrInvoiceList: FC<DistributorOrInvoiceListProps> = ({
+  title = '',
+  date = '',
+  amount = '',
+  seleceted,
+  distributorId,
+  onClick = () => {},
+}) => {
+  return (
+    <div
+      onClick={() => onClick(distributorId)}
+      className={`flex items-center justify-between px-4 py-4 border-b border-border cursor-pointer hover:bg-grayLight transition border-l-4 border-l-primary hover:bg-shade-blue ${
+        seleceted ? 'bg-shade-blue' : ''
+      }`}
+    >
+      <div>
+        <h3 className="text-sm font-semibold mb-1">{title}</h3>
+        <p className="text-sm text-gray">{date}</p>
+      </div>
+      <div className="font-bold text-danger">₹{amount}</div>
+    </div>
+  );
+};
+
+export default DistributorOrInvoiceList;
--- src/app/dashboard/purchases/page.tsx
+++ src/app/dashboard/purchases/page.tsx
@@ -1,104 +1,102 @@
 'use client';
-import { FaCartPlus } from 'react-icons/fa';
-import { MdOutlineContentPasteSearch } from 'react-icons/md';
-import { TbSortAscending } from 'react-icons/tb';
-
 import React, { useState } from 'react';
+import { IoMdSearch } from 'react-icons/io';
+import { LuCalendarArrowUp } from 'react-icons/lu';
+import { FaRegAddressBook } from 'react-icons/fa';
+import { TbSortAscending } from 'react-icons/tb';
+import { IoChevronBackOutline } from 'react-icons/io5';
+
+type selectedDistributorType = {
+  selcted: boolean;
+  distributerId: number | null;
+  distributorInvoiceData: string[];
+};
+
+/** components */
 import { SearchBar } from '@/components/ui/searchbar';
+import DistributorOrInvoiceList from './component/distributor-or-invoice-list';
 
 const PurchaseContainer = () => {
+  /** Required states */
   const [query, setQuery] = useState('');
+  const [selectedDistributor, setSelectedDistributor] =
+    useState<selectedDistributorType>({
+      selcted: false,
+      distributerId: null,
+      distributorInvoiceData: [],
+    });
 
   return (
     <div className="w-full bg-white rounded-lg h-full flex">
-      <div className="w-96 border-r-1 h-full border-border">
+      <div className="w-md border-r-1 h-full border-border">
         <div className="flex justify-between items-center p-4">
-          <div>
-            <h3 className="text-xl font-bold">Purchases</h3>
-            <p className="text-gray text-md">View or edit purchases</p>
+          <div className="flex items-center gap-4">
+            {selectedDistributor.selcted && <IoChevronBackOutline size={25} />}
+            <div>
+              <h3 className="text-xl font-bold">Purchases</h3>
+              <p className="text-gray text-md">View or Edit Purchases</p>
+            </div>
           </div>
           <div className="flex items-center px-4 py-2 bg-primary text-white rounded-lg cursor-pointer hover:opacity-90 transition">
-            <FaCartPlus className="mr-2" />
-            Add Purchases
+            <FaRegAddressBook className="mr-2" />
+            Add Distributors
           </div>
         </div>
 
         <div className="flex items-center gap-4 border-b border-border pb-6 p-4">
           <SearchBar
-            placeholder="Search Purchases..."
+            placeholder="Search by Name or GSTIN"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
-            icon={
-              <MdOutlineContentPasteSearch size={20} className="text-black" />
-            }
+            className="text-semibold"
+            icon={<IoMdSearch size={20} className="text-black" />}
           />
+          <div className="border border-border rounded-lg p-2 hover:bg-grayLight transition bg-bg-secondary">
+            <LuCalendarArrowUp
+              size={23}
+              className="text-black cursor-pointer"
+            />
+          </div>
           <div className="border border-border rounded-lg p-2 hover:bg-grayLight transition bg-bg-secondary">
             <TbSortAscending size={23} className="text-black cursor-pointer" />
           </div>
         </div>
-
-        <div className="flex items-center justify-between px-4 py-2 border-b border-border cursor-pointer hover:bg-grayLight transition border-l-4 border-l-primary hover:bg-gray-200">
-          <div>
-            <h3 className="text-sm font-semibold">
-              Sample Distributor (B1-32564)
-            </h3>
-            <p className="text-sm text-gray">Last Txn: 12/11/25</p>
-          </div>
-          <div className="font-bold text-black">₹9000</div>
+        <div>
+          <h3 className="font-medium text-sm px-4 py-2 pl-4 border-b border-border bg-shade-yellow">
+            {!selectedDistributor.selcted ? (
+              <>
+                Select a distributor to view there&nbsp;
+                <span className="underline font-semibold">Invoices</span>
+              </>
+            ) : (
+              <>
+                Select a invoice to get&nbsp;
+                <span className="underline font-semibold">
+                  Purchase Details
+                </span>
+              </>
+            )}
+          </h3>
         </div>
 
-        <div className="flex items-center justify-between px-4 py-2 border-b border-border cursor-pointer hover:bg-grayLight transition border-l-4 border-l-primary hover:bg-gray-200">
-          <div>
-            <h3 className="text-sm font-semibold">
-              Abinash Distributor (B1-32564)
-            </h3>
-            <p className="text-sm text-gray">Last Txn: 21/02/25</p>
-          </div>
-          <div className="font-bold text-black">₹5400</div>
-        </div>
-
-        <div className="flex items-center justify-between px-4 py-2 border-b border-border cursor-pointer hover:bg-grayLight transition border-l-4 border-l-primary hover:bg-gray-200">
-          <div>
-            <h3 className="text-sm font-semibold">
-              Keonjhar Distributor (B1-32564)
-            </h3>
-            <p className="text-sm text-gray">Last Txn: 14/02/25</p>
-          </div>
-          <div className="font-bold text-black">₹3800</div>
-        </div>
-        <div className="flex items-center justify-between px-4 py-2 border-b border-border cursor-pointer hover:bg-grayLight transition border-l-4 border-l-primary hover:bg-gray-200">
-          <div>
-            <h3 className="text-sm font-semibold">
-              Sample Distributor (B1-32564)
-            </h3>
-            <p className="text-sm text-gray">Last Txn: 12/11/25</p>
-          </div>
-          <div className="font-bold text-black">₹9000</div>
-        </div>
-
-        <div className="flex items-center justify-between px-4 py-2 border-b border-border cursor-pointer hover:bg-grayLight transition border-l-4 border-l-primary hover:bg-gray-200">
-          <div>
-            <h3 className="text-sm font-semibold">
-              Abinash Distributor (B1-32564)
-            </h3>
-            <p className="text-sm text-gray">Last Txn: 21/02/25</p>
-          </div>
-          <div className="font-bold text-black">₹5400</div>
-        </div>
-
-        <div className="flex items-center justify-between px-4 py-2 border-b border-border cursor-pointer hover:bg-grayLight transition border-l-4 border-l-primary hover:bg-gray-200">
-          <div>
-            <h3 className="text-sm font-semibold">
-              Keonjhar Distributor (B1-32564)
-            </h3>
-            <p className="text-sm text-gray">Last Txn: 14/02/25</p>
-          </div>
-          <div className="font-bold text-black">₹3800</div>
-        </div>
+        <DistributorOrInvoiceList
+          title="Sample Distributor (B1-32564)"
+          date="Last Txn: 12/11/25"
+          amount={25000}
+          distributorId={1}
+          seleceted={selectedDistributor.distributerId === 1}
+          onClick={(id) =>
+            setSelectedDistributor({
+              selcted: true,
+              distributerId: id,
+              distributorInvoiceData: [],
+            })
+          }
+        />
       </div>
 
-      <div className="h-full flex-1 p-4 ">
-        <p>bakchodi</p>
+      <div className="h-full flex-1 p-4">
+        <p>Lodu lallit</p>
       </div>
     </div>
   );
